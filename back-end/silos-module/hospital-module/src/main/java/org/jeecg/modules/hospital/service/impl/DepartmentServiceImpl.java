9d7c4440d2b54c1cbb053b114cb62ad3c89c4fec
zsq <2877205867@qq.com>
2025-10-11 19:51:59 +0800

    科室内容，新增web

--- back-end/silos-module/hospital-module/src/main/java/org/jeecg/modules/hospital/service/impl/DepartmentServiceImpl.java
+++ back-end/silos-module/hospital-module/src/main/java/org/jeecg/modules/hospital/service/impl/DepartmentServiceImpl.java
@@ -1,116 +1,37 @@
-package org.jeecg.modules.hospital.service.impl;
+package org.jeecg.modules.hospital.common;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.jeecg.modules.hospital.exception.BusinessException;
-import org.jeecg.modules.hospital.common.ErrorCode;
-import org.jeecg.modules.hospital.entity.Department;
-import org.jeecg.modules.hospital.entity.Doctor;
-import org.jeecg.modules.hospital.mapper.DepartmentMapper;
-import org.jeecg.modules.hospital.mapper.DoctorMapper;
-import org.jeecg.modules.hospital.service.DepartmentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
+import lombok.Getter;
 
 /**
- * 科室表服务实现
- * @author Administrator
- * @description 针对表【department(科室表)】的数据库操作Service实现
- * @createDate 2025-09-22 20:15:20
+ * 错误码
+ *
  */
-@Service
-public class DepartmentServiceImpl extends ServiceImpl<DepartmentMapper, Department> implements DepartmentService {
+@Getter
+public enum ErrorCode {
 
-    @Autowired
-    private DepartmentMapper departmentMapper;
+    SUCCESS(0, "ok", ""),
+    PARAMS_ERROR(40000, "请求参数错误", ""),
+    NULL_ERROR(40001, "请求数据为空", ""),
+    NOT_LOGIN(40100, "未登录", ""),
+    NO_AUTH(40101, "无权限", ""),
+    SYSTEM_ERROR(50000, "系统内部异常", "");
 
-    @Autowired
-    private DoctorMapper doctorMapper;
+    private final int code;
 
-    @Override
-    public List<Department> getFirstLevelDepartments() {
-        return departmentMapper.selectFirstLevelDepartments();
+    /**
+     * 状态码信息
+     */
+    private final String message;
+
+    /**
+     * 状态码描述（详情）
+     */
+    private final String description;
+
+    ErrorCode(int code, String message, String description) {
+        this.code = code;
+        this.message = message;
+        this.description = description;
     }
 
-    @Override
-    public List<Department> getSecondLevelByParentId(Long parentDeptId) {
-        if (parentDeptId == null) {
-            throw new BusinessException(ErrorCode.PARAMS_ERROR, "父科室ID不能为空");
-        }
-        return departmentMapper.selectSecondLevelByParentId(parentDeptId);
-    }
-
-    @Override
-    public List<Map<String, Object>> getDepartmentTree() {
-        return departmentMapper.selectDepartmentWithChildren();
-    }
-
-    @Override
-    public boolean createDepartment(Department department) {
-        // 校验科室名称唯一性
-        QueryWrapper<Department> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("dept_name", department.getDeptName());
-        if (baseMapper.selectCount(queryWrapper) > 0) {
-            throw new BusinessException(ErrorCode.PARAMS_ERROR, "科室名称已存在");
-        }
-
-        // 设置时间
-        department.setCreateTime(LocalDateTime.now());
-        department.setUpdateTime(LocalDateTime.now());
-        return save(department);
-    }
-
-    @Override
-    public boolean updateDepartment(Department department) {
-        // 校验科室存在性
-        if (getById(department.getDeptId()) == null) {
-            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR, "科室不存在");
-        }
-
-        // 校验名称唯一性（排除自身）
-        QueryWrapper<Department> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("dept_name", department.getDeptName())
-                .ne("dept_id", department.getDeptId());
-        if (baseMapper.selectCount(queryWrapper) > 0) {
-            throw new BusinessException(ErrorCode.PARAMS_ERROR, "科室名称已存在");
-        }
-
-        department.setUpdateTime(LocalDateTime.now());
-        return updateById(department);
-    }
-
-    @Override
-    public boolean deleteDepartment(Long deptId) {
-        // 检查是否有医生关联
-        QueryWrapper<Doctor> doctorQueryWrapper = new QueryWrapper<>();
-        doctorQueryWrapper.eq("dept_id", deptId);
-        if (doctorMapper.selectCount(doctorQueryWrapper) > 0) {
-            throw new BusinessException(ErrorCode.OPERATION_ERROR, "该科室下有关联医生，无法删除");
-        }
-
-        // 检查是否有子科室（如果是一级科室）
-        QueryWrapper<Department> childQueryWrapper = new QueryWrapper<>();
-        childQueryWrapper.eq("parent_dept_id", deptId);
-        if (baseMapper.selectCount(childQueryWrapper) > 0) {
-            throw new BusinessException(ErrorCode.OPERATION_ERROR, "该科室下有子科室，无法删除");
-        }
-
-        return removeById(deptId);
-    }
-
-    @Override
-    public List<Department> searchDepartments(String keyword) {
-        QueryWrapper<Department> queryWrapper = new QueryWrapper<>();
-        queryWrapper.like("dept_name", keyword)
-                .or().like("dept_desc", keyword);
-        return baseMapper.selectList(queryWrapper);
-    }
 }
-
-
-
-
