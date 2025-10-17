ee8d7955924f69ae87e7c14b9c03870b086266ff
yeye <queenmoon@foxmail.com>
2025-10-17 21:29:43 +0800

    恢复用户相关后端为10.5号完成权限校验版本，其它未改动

--- back-end/silos-module/hospital-module/src/main/java/org/jeecg/modules/hospital/mapper/DepartmentMapper.java
+++ back-end/silos-module/hospital-module/src/main/java/org/jeecg/modules/hospital/mapper/DepartmentMapper.java
@@ -1,36 +1,16 @@
 package org.jeecg.modules.hospital.mapper;
 
-import com.baomidou.dynamic.datasource.annotation.DS;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.jeecg.modules.hospital.entity.Department;
-
-import java.util.List;
-import java.util.Map;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
- * 科室表数据库操作Mapper
- * @author Administrator
- * @description 针对表【department(科室表)】的数据库操作Mapper
- * @createDate 2025-09-22 20:15:20
- * @Entity org.jeecg.modules.hospital.entity.Department
- */
-@DS("hospital")
+* @author Administrator
+* @description 针对表【department(科室表)】的数据库操作Mapper
+* @createDate 2025-09-22 20:15:20
+* @Entity org.jeecg.modules.hospital.entity.Department
+*/
 public interface DepartmentMapper extends BaseMapper<Department> {
 
-    /**
-     * 查询所有一级科室
-     */
-    List<Department> selectFirstLevelDepartments();
-
-    /**
-     * 根据父科室ID查询二级科室
-     */
-    List<Department> selectSecondLevelByParentId(Long parentDeptId);
-
-    /**
-     * 查询科室及下属二级科室
-     */
-    List<Map<String, Object>> selectDepartmentWithChildren();
 }
 
 
