e3e02b20180afbb203ef5ea0109bef4faf6acbec
zjy314315 <zhengjiayi314@qq.com>
2025-10-31 15:26:10 +0800

    备份

--- 校医院挂号系统/unpackage/dist/dev/mp-weixin/common/vendor.js
+++ 校医院挂号系统/unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -7058,7 +7058,7 @@ function isConsoleWritable() {
 function initRuntimeSocketService() {
   const hosts = "10.60.205.245,127.0.0.1";
   const port = "8090";
-  const id = "mp-weixin_gPwqzT";
+  const id = "mp-weixin_U96yY_";
   const lazy = typeof swan !== "undefined";
   let restoreError = lazy ? () => {
   } : initOnError();
