1fd26a055f902a11b9541fa43ed63633c9d682da
just <2877205867@qq.com>
2025-11-23 13:23:30 +0800

    优化科室(Department)相关文件：添加自动字段填充和数据源切换注解

--- back-end/silos-module/hospital-module/src/main/java/org/jeecg/modules/hospital/entity/Department.java
+++ back-end/silos-module/hospital-module/src/main/java/org/jeecg/modules/hospital/entity/Department.java
@@ -1,6 +1,8 @@
 package org.jeecg.modules.hospital.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.time.LocalDateTime;
@@ -47,11 +49,13 @@ public class Department {
     /**
      * 创建时间
      */
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
     /**
      * 更新时间
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
     @Override
